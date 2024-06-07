9bd7ba3859b9d9ae4edcde5048d9fef07aa885fb
lleballex <lleballex@yandex.ru>
2024-06-07 17:30:03 +0300

    update vacancy status types

--- frontend/src/types/entities/vacancy.ts
+++ frontend/src/types/entities/vacancy.ts
@@ -1,6 +1,18 @@
 import { BaseEntity } from '@/types/entities/base-entity'
+import { Recruiter } from '@/types/entities/recruiter'
 
-export enum VacancyStatus {}
+export enum VacancyStatus {
+  Active = 'active',
+  Closed = 'closed',
+}
+
+export const VacancyStatuses: Record<
+  VacancyStatus,
+  { title: string; color: string }
+> = {
+  [VacancyStatus.Active]: { title: 'Активна', color: 'green' },
+  [VacancyStatus.Closed]: { title: 'Закрыта', color: 'gray-4' },
+}
 
 export interface Vacancy extends BaseEntity {
   title: string
@@ -15,11 +27,13 @@ export interface Vacancy extends BaseEntity {
   responces: string[] // TODO
   skills: string[] // TODO
   recruiter_id: string
+  recruiter?: Recruiter // TODO
   work_experience: string
   work_type_id: string
   work_schedule_id: string
   scope_id: string
   stages: string[] // TODO
+  creation_date: string
 }
 
 export interface GetVacanciesParams {}
