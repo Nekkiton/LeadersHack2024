141591912b9cb604ce11c3f447d5f6c9eedfd913
lleballex <lleballex@yandex.ru>
2024-06-08 20:29:27 +0300

    update CandidateHeader

--- frontend/src/components/main/candidate/Candidate/CandidateHeader/CandidateHeader.tsx
+++ frontend/src/components/main/candidate/Candidate/CandidateHeader/CandidateHeader.tsx
@@ -1,4 +1,9 @@
 import { Candidate } from '@/types/entities/candidate'
+import { getUserName } from '@/lib/get-user-name'
+import { getUserAge } from '@/lib/get-user-age'
+import Button from '@/components/ui/Button'
+import Image from '@/components/ui/Image'
+import userImg from '@/assets/images/user.png'
 import styles from './CandidateHeader.module.scss'
 
 interface Props {
@@ -6,5 +11,32 @@ interface Props {
 }
 
 export default function CandidateHeader({ candidate }: Props) {
-  return <div></div>
+  return (
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <Image
+          className={styles.userImg}
+          src={candidate.photo ?? userImg}
+          width={200}
+          height={200}
+        />
+        <div className={styles.userInfo}>
+          <h1>{getUserName(candidate, 'Name Surname')}</h1>
+          <div className={styles.userInfoData}>
+            <p>{candidate.job_title}</p>
+            {candidate.birthday && (
+              <p>
+                {getUserAge(candidate.birthday)}, {candidate.city}
+              </p>
+            )}
+            <p>TODO</p>
+          </div>
+        </div>
+      </div>
+      {/* TODO: action */}
+      <Button type="primary" onClick={() => alert('comming soon')}>
+        Пригласть на вакансию
+      </Button>
+    </div>
+  )
 }
