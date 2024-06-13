e2d30867493b3d22ee7b2c79f64a0dda7b8d3686
lleballex <lleballex@yandex.ru>
2024-06-13 22:18:23 +0300

    increase access token duration

--- api/app/settings.py
+++ api/app/settings.py
@@ -6,7 +6,7 @@ class Settings(EnvClass):
     MONGO_CONNECTION_STRING: str
 
     REFRESH_TOKEN_EXPIRES_IN: int = 90*24*60*60
-    ACCESS_TOKEN_EXPIRES_IN: int = 15*60
+    ACCESS_TOKEN_EXPIRES_IN: int = 24*60*60
     JWT_ALGORITHM: str = "RS256"
     JWT_PUBLIC_KEY_PATH: str = "/code/public.key.pub"
     JWT_PRIVATE_KEY_PATH: str = "/code/private.key"
