2ed0a715894bf556cdacdf2e2ac5a43d953954af
docryte <docryte@gmail.com>
2024-06-14 10:50:50 +0300

    Функция-заглушка для получения url встречи

--- api/app/utils.py
+++ api/app/utils.py
@@ -1,6 +1,5 @@
-from pydantic import BaseModel, create_model
 from datetime import datetime, timezone
-from typing import Optional, Type
+from typing import Literal
 import bcrypt
 
 
@@ -23,3 +22,13 @@ def validate_password(password: str, hashed_password: str) -> bool:
     Сверяет обычный пароль с хэшированным
     """
     return bcrypt.checkpw(password.encode(), hashed_password.encode())
+
+
+def get_meet_url(platform: Literal["telemost", "googlemeet", "zoom"], date: datetime) -> str:
+    match platform:
+        case "telemost":
+            return ""
+        case "googlemeet":
+            return ""
+        case "zoom":
+            return ""
