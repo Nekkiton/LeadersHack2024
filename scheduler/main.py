9b9b0580660b00d8daa5eb154cecc87527977caa
nekkiton <79106908386@yandex.ru>
2024-06-15 02:31:15 +0300

    add scheduler to infra

--- scheduler/main.py
+++ scheduler/main.py
@@ -6,7 +6,6 @@ logging.basicConfig(
     level=logging.INFO,
     format="[%(asctime)s] [%(levelname)s] %(message)s",
     datefmt="%d.%m.%Y] [%H:%M:%S",
-    stream=sys.stdout
 )
 
 if __name__ == "__main__":
