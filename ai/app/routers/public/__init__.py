f6f972c063cc5cad0e8c31628b3c756e23b29f12
nekkiton <79106908386@yandex.ru>
2024-06-15 00:47:23 +0300

    update validation

--- ai/app/routers/public/__init__.py
+++ ai/app/routers/public/__init__.py
@@ -1,7 +1,9 @@
 import json
+from typing import get_args
 from fastapi import APIRouter, UploadFile
 from app.ai import parse_cv, process_vacancy
 from app.schemas.vacancies import VacancyPartial
+from app.literals import WorkSchedules, WorkExperiences, WorkTypes, WorkScopes, Skills
 
 router = APIRouter(tags=["Публичное"], prefix="/public")
 
@@ -23,4 +25,10 @@ async def analyse_candidate_cv(file: UploadFile):
 async def process_vacanсy(vacancy: VacancyPartial):
     vacancy = process_vacancy(vacancy)
     json_vacancy = json.loads(vacancy)
+
+    json_vacancy['work_schedule'] = json_vacancy['work_schedule'] if json_vacancy['work_schedule'] in get_args(WorkSchedules) else "5/2"
+    json_vacancy['work_type'] = json_vacancy['work_type'] if json_vacancy['work_type'] in get_args(WorkTypes) else "Удаленно"
+    json_vacancy['work_experience'] = json_vacancy['work_experience'] if json_vacancy['work_experience'] in get_args(WorkExperiences) else "Нет опыта"
+    json_vacancy['skills'] = [skill for skill in json_vacancy['skills'] if skill in get_args(Skills)]
+
     return json_vacancy
