c30769ca5459b71dedd527f914f7ab19baac5782
docryte <docryte@gmail.com>
2024-06-14 10:17:50 +0300

    fix

--- api/app/routers/recruiter/responses/aggregations.py
+++ api/app/routers/recruiter/responses/aggregations.py
@@ -1,38 +1,34 @@
-PAGINATED_MATCH_RESPONSES = lambda vacancy_id, page, limit: [
-    [
-        {
-            "$match": {
-                "vacancy_id": vacancy_id
-            }
-        },
-        {
-            "$facet": {
-                "0": [{"$count": "c"}],
-                "5": [{"$match": {"match": {"$gte": 50}}}, {"$count": "c"}],
-                "7": [{"$match": {"match": {"$gte": 75}}}, {"$count": "c"}],
-                "9": [{"$match": {"match": {"$gte": 90}}}, {"$count": "c"}],
-                "items": [{"$skip": page * limit}, {"$limit": limit}]
-            }
-        },
-        {
-            "$project": {
-                "match": {
-                    "all": {"$first": "$0.c"},
-                    "gte50": {"$first": "$5.c"},
-                    "gte70": {"$first": "$7.c"},
-                    "gte90": {"$first": "$9.c"},
-                },
-                "total_pages": {
-                    "$trunc": [{
-                        "$divide": [
-                            {"$first": "$0.c"},
-                            limit
-                        ]
-                    }]
-                },
-                "page": {"$toInt": page},
-                "items": 1,
-            }
+PAGINATED_MATCH_RESPONSES = lambda query, page, limit: [
+    {
+        "$match": query
+    },
+    {
+        "$facet": {
+            "0": [{"$count": "c"}],
+            "5": [{"$match": {"match": {"$gte": 50}}}, {"$count": "c"}],
+            "7": [{"$match": {"match": {"$gte": 75}}}, {"$count": "c"}],
+            "9": [{"$match": {"match": {"$gte": 90}}}, {"$count": "c"}],
+            "items": [{"$skip": page * limit}, {"$limit": limit}]
         }
-    ]
+    },
+    {
+        "$project": {
+            "match": {
+                "all": {"$first": "$0.c"},
+                "gte50": {"$first": "$5.c"},
+                "gte70": {"$first": "$7.c"},
+                "gte90": {"$first": "$9.c"}
+            },
+            "total_pages": {
+                "$trunc": [{
+                    "$divide": [
+                        {"$first": "$0.c"},
+                        limit
+                    ]
+                }]
+            },
+            "page": {"$toInt": page},
+            "items": 1,
+        }
+    }
 ]
