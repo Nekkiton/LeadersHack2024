c253989a0723879c01172637c1229ec5e45f571d
nekkiton <79106908386@yandex.ru>
2024-06-15 13:45:13 +0300

    update parser

--- scheduler/app/app.py
+++ scheduler/app/app.py
@@ -11,15 +11,15 @@ def run() -> None:
     """
     schedule = Scheduler()
     # schedule.daily(
-    #     timing=time(hour=11, minute=00),
+    #     timing=time(hour=21, minute=00),
     #     handle=parse_vacancies_from_rntgroup,
 
     #     )
     schedule.hourly(
-        timing=time(minute=20, second=0),
+        timing=time(minute=10, second=0),
         handle=parse_vacancies_from_rntgroup,
-
         )
+
     logging.info("Планировщик запущен")
     while True:
         schedule.exec_jobs()
--- scheduler/app/database.py
+++ scheduler/app/database.py
@@ -6,5 +6,7 @@ mongo_client = pymongo.MongoClient(Settings.MONGO_CONNECTION_STRING)
 mongo_db = mongo_client[Settings.MONGO_DATABASE]
 
 Vacancies = mongo_db.get_collection("vacancies")
+Stages = mongo_db.get_collection("stages")
+Users = mongo_db.get_collection("users")
 
 logging.info("Подключение к MongoDB установлено")
