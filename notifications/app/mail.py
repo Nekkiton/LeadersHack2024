498aaf2aebc62f3b5dca9db4170809827111e770
docryte <docryte@gmail.com>
2024-06-15 15:12:56 +0300

    FIX

--- notifications/app/mail.py
+++ notifications/app/mail.py
@@ -6,8 +6,8 @@ from app.settings import Settings
 Sender = EmailSender(
     host=Settings.EMAIL_HOST,
     port=Settings.EMAIL_PORT,
-    username=Settings.EMAIL_USERNAME,
-    password=Settings.EMAIL_PASSWORD,
+    username=Settings.EMAIL_USER,
+    password=Settings.EMAIL_PASS,
     cls_smtp=SMTP_SSL,
     use_starttls=False
 )
